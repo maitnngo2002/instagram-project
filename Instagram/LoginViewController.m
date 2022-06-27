3ce6ff7cd9f9c8fc223a3e187d014958f0e1a076
maingo <ngothingocmaiclht@gmail.com>
2022-06-27 13:44:21 -0700

    enable users to sign up and log in

--- Instagram/LoginViewController.m
+++ Instagram/LoginViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/Parse.h"
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -17,7 +20,83 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)onTapRegister:(id)sender {
+    [self registerUser];
+}
+- (IBAction)onTapLogin:(id)sender {
+    [self loginUser];
+}
+- (void)registerUser {
+// initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            self.usernameField.text = @"";
+            self.passwordField.text = @"";
+        }
+    }];
+}
+- (void)loginUser {
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""] ) {
+        [self showAlert:@""];
+        return;
+    }
+    
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+            [self showAlert:error.localizedDescription];
+        } else {
+            NSLog(@"User logged in successfully");
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
 
+-(void)showAlert:(NSString *) errorString {
+    UIAlertController *alert;
+   
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    
+    if ([errorString isEqual:@""]) {
+        alert = [UIAlertController alertControllerWithTitle:@"Missing Fields"
+                                                    message:@"Username or/and password field empty, try again."
+                                             preferredStyle:(UIAlertControllerStyleAlert)];
+        [alert addAction:cancelAction];
+    }
+    else {
+        alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                    message:errorString
+                                             preferredStyle:(UIAlertControllerStyleAlert)];
+        [alert addAction:okAction];
+    }
+    
+    [self presentViewController:alert animated:YES completion:^{
+        NSLog(@"Finished presenting alert");
+    }];
+}
 /*
 #pragma mark - Navigation
 
