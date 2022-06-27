0e1d20bd9f2e5d4e6a3e576f4a21bfc43099b5e8
maingo <ngothingocmaiclht@gmail.com>
2022-06-27 13:34:06 -0700

    set up view controllers

---
+++ Instagram/ComposeViewController.m
@@ -0,0 +1,36 @@
+//
+//  ComposeViewController.m
+//  Instagram
+//
+//  Created by Mai Ngo on 6/27/22.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)didTapCancel:(id)sender {
+}
+
+- (IBAction)didTapShare:(id)sender {
+}
+
+@end
---
+++ Instagram/HomeFeedViewController.m
@@ -0,0 +1,35 @@
+//
+//  HomeFeedViewController.m
+//  Instagram
+//
+//  Created by Mai Ngo on 6/27/22.
+//
+
+#import "HomeFeedViewController.h"
+
+@interface HomeFeedViewController ()
+
+@end
+
+@implementation HomeFeedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)didTapLogout:(id)sender {
+}
+- (IBAction)didTapCameraIcon:(id)sender {
+}
+
+@end
